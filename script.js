09272cf1ad17171ed220721cc97d2d87a140b76d
SkolotajsRaivis <71266644+raivis-rasnacs@users.noreply.github.com>
2022-01-23 12:42:29 +0000

    Major bugs fixed

--- script.js
+++ script.js
@@ -12,15 +12,17 @@ const words = [
   "OZOLS",
   "TEIKA"
 ]
-word = "OZOLS";
-//var word = words[Math.floor(Math.random()*words.length)];
-document.getElementById("guess1").firstElementChild.innerHTML = word.slice(0,1);
-console.log(word);
+
+var wordInArray = [];
+var usedWords = [];
+var word = "";
 var guesses = 0;
 var currentWord = "";
 var lives = 3;
+var score = 0;
 correctLettersInCorrectPosition = [];
 correctLettersInWrongPosition = [];
+newWord();
 
 document.getElementById("letter5").addEventListener("keyup", function(event) {
   var inputs = document.getElementsByTagName("input");
@@ -38,23 +40,37 @@ document.getElementById("letter5").addEventListener("keyup", function(event) {
 });
 
 function checkWord(currentWord) {
+  var cells = document.getElementById("guess"+guesses).cells;
+  if (guesses == 1) { document.getElementById("guess1").firstElementChild.innerHTML = ""; }
   for (var i = 0; i < currentWord.length; i++) {
     for (var j = 0; j < word.length; j++) {
       if (currentWord[i] == word[j] && i == j) {
-        correctLettersInCorrectPosition.push(i);
-        continue;
-      }
-      else if (currentWord[i] == word[j]) {
-        correctLettersInWrongPosition.push(i);
-        continue;
+        if (wordInArray.includes(currentWord[i])) {
+        correctLettersInCorrectPosition.push(i); 
+        cells[i].innerHTML = word.slice(i, i+1); 
+        wordInArray[i] = ""; }
       }
     }
   }
-  
-  if (currentWord == word) { //Pārbauda, vai uzminēts vārds
-    alert("Uzminēji!"); newWord(); 
+
+  for (var i = 0; i < wordInArray.length; i++) {
+    if (wordInArray[i] != "") {
+      for (var j = i; j < wordInArray.length; j++) {
+        if (wordInArray[j] != "") {
+          if (wordInArray.includes(currentWord[i])) {
+            correctLettersInWrongPosition.push(i); 
+            cells[i].innerHTML = currentWord.slice(i, i+1); 
+            wordInArray[wordInArray.indexOf(currentWord[i])] = "X";
+          } break;
+        }
+      }
     }
-  else if (guesses == 6) { //Zaudē dzīvību
+  }
+
+  if (currentWord == word) { //Pārbauda, vai uzminēts vārds
+    alert("Uzminēji!"); newWord(); score += 10; 
+    }
+  else if (guesses == 5) { //Zaudē dzīvību
     if (lives == 0) { alert("Zaudēji!"); lives = 3; setHearts(); newWord(); } //Jauna spēle
     else {
       lives--;
@@ -68,17 +84,11 @@ function checkWord(currentWord) {
   }
   correctLettersInCorrectPosition = [];
   correctLettersInWrongPosition = [];
-}
 
-function formWordFromFields() {
-currentWord = "";
-  var inputs = document.getElementsByTagName("input");
-  for (input of inputs) {
-    currentWord = currentWord + input.value;
-    currentWord = currentWord.toUpperCase();
+  wordInArray = [];
+  for (letter of word) {
+    wordInArray.push(letter);
   }
-  //alert(currentWord);
-  checkWord(currentWord);
 }
 
 function jump(currentInput) {
@@ -92,11 +102,9 @@ function colorTable() {
   for (var i = 0; i < cells.length; i++) {
     if (correctLettersInWrongPosition.includes(i)) {
       cells[i].style.backgroundColor = "yellow";
-      cells[i].innerHTML = word.slice(i, i+1);
     }
     else if (correctLettersInCorrectPosition.includes(i)) {
       cells[i].style.backgroundColor = "red";
-      cells[i].innerHTML = word.slice(i, i+1);
     }
     else { cells[i].style.backgroundColor = "green"; }
   }
@@ -111,18 +119,40 @@ function clearInputs() {
 }
 
 function newWord() {
+  if (guesses != 0) {
   clearInputs();
-  word = words[Math.floor(Math.random()*words.length)];
-  console.log(word);
-  var rows = document.getElementsByTagName("TBODY")[0].children;
-  for (var i = 0; i < 5; i++) {
-    var cells = rows[i].children;
-    for (cell of cells) {
-      cell.innerHTML = "";
-      cell.style.backgroundColor = "white";
-    }
+  clearGameField();
   }
+
+  if (usedWords.length < words.length) {
+  do {
+    word = words[Math.floor(Math.random()*words.length)];
+  }
+  while (usedWords.includes(word));
+  usedWords.push(word);
+  }
+  else { alert("Atminēji visus vārdus!"); }
+
+  document.getElementById("guess1").firstElementChild.innerHTML = word.slice(0,1);
+
+  wordInArray = [];
+  for (letter of word) {
+    wordInArray.push(letter);
+  }
+
+  console.log(wordInArray);
+  console.log(word);
   guesses = 0;
+}
+
+function formWordFromFields() {
+currentWord = "";
+  var inputs = document.getElementsByTagName("input");
+  for (input of inputs) {
+    currentWord = currentWord + input.value;
+  }
+  currentWord = currentWord.toUpperCase();
+  checkWord(currentWord);
 }
 
 function setHearts() {
@@ -133,5 +163,16 @@ function setHearts() {
 
   for (var i = 0; i < lives; i++) {
     hearts[i].style.visibility = "visible";
+  }
+}
+
+function clearGameField() {
+var rows = document.getElementsByTagName("TBODY")[0].children;
+  for (var i = 0; i < 5; i++) {
+    var cells = rows[i].children;
+    for (cell of cells) {
+      cell.innerHTML = "";
+      cell.style.backgroundColor = "white";
+    }
   }
 }
